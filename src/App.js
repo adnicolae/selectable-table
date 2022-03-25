2c32bd167439b8b66bd9ef95e3e07ee3e065239a
Andrei Nicolae <ad.nicolae@outlook.com>
2022-03-25 07:50:08 +0200

    Add helper packages, eslint and prettier configs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
